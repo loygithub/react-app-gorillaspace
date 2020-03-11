bda1fa63a3743f82c0dd6c4376a97e004823f8c4
Lawrence Anthony Cruz <lawrenceanthony.cruz@yahoo.com>
2020-03-11 17:07:48 +0800

    initial commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div className="landingpage-container">
+            <div className="Find-your-ideal-workspace-or-venue-We-are-now-in-2-cities">
+                <span>Find your ideal workspace or venue. We are now in 2 cities.</span>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
